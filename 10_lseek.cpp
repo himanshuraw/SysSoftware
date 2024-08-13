4b70ad3a391e04c21f3e0e50c73bdd4104d3fae4
himanshuraw <himanshurawat180@gmail.com>
2024-08-13 23:50:31 +0530

    till 10 + gitignore

--- 10_lseek.cpp
+++ 10_lseek.cpp
@@ -1,12 +1,30 @@
 #include<iostream>
 #include<unistd.h>
+#include<fcntl.h>
+
+using namespace std;
 
 int main(int argc, char** argv){
 	
 	if(argc < 2){
 		cerr << "Please input appropriate arguments";
-		rteurn -1;
+		return -1;
 	}
 
+	int fd = open(argv[1], O_CREAT | O_RDWR);
+	char buff[] = "0123456789";
+
+	int count = write(fd, buff, 10);
+	if(count == -1){
+		cerr << "Error in writing `I`";
+	}
+
+	auto val = lseek(fd, 10, SEEK_CUR);
+	cout<<"lseek returned : "<< val<<endl;
+
+	count = write(fd, buff, 10);
+	if(count == -1){
+		cerr << "Error in writing `I`";
+	}
 
 }
