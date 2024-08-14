4dbc448cb94e168540383d27824fb92020dd24c0
himanshuraw <himanshurawat180@gmail.com>
2024-08-14 17:16:58 +0530

    Finished till 12

---
+++ 12_getMode.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<fcntl.h>
+#include<unistd.h>
+
+using namespace std;
+
+int main(int argc, char** argv){
+	if(argc < 2){
+		cerr<<"Need more arguments!!";
+		return -1;
+	}
+
+	int fd = open(argv[1], O_CREAT | O_RDONLY);
+	if(fd == -1){
+		cerr<<"Error in opening the file.";
+		return -1;
+	}
+
+	int mode = fcntl(fd, F_GETFL, 0);
+	if(mode == -1){
+		cerr<<"Error in getting the mode of the file.";
+		close(fd);
+		return -1;
+	}
+
+	int aMode = mode & O_ACCMODE;
+
+	switch(aMode){
+		case O_RDONLY:	cout<<"Read only.";
+			      	break;
+		case O_WRONLY:	cout<<"Write only.";
+			     	break;
+		case O_RDWR:	cout<<"Read write.";
+				break;
+		default:	cerr<<"Unknown";
+				close(fd);
+				return -1;
+	}
+
+	close(fd);
+}
