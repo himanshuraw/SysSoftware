83fd09399c80ef2b9975ecc28141597faed584ea
himanshuraw <himanshurawat180@gmail.com>
2024-09-17 10:52:10 +0530

    HandsOn 2: 10

---
+++ HandsOn-2/10b.c
@@ -0,0 +1,45 @@
+/*
+========================================================================================================
+Name : 10b
+Author : Himanshu Rawat
+Description : Write a separate program using sigaction system call to catch the following signals.
+		b. SIGINT
+Date: 17 Sept, 2024.
+========================================================================================================
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<unistd.h>
+
+void sigint_handler(int signo) {
+    printf("Caught SIGINT (Interrupt signal)\n");
+    exit(1);
+}
+
+int main() {
+    struct sigaction sa;
+    sa.sa_handler = sigint_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    sigaction(SIGINT, &sa, NULL);
+
+    printf("Press Ctrl+C to trigger SIGINT...\n");
+    while (1) {
+        pause();
+    }
+}
+
+/*
+========================================================================================================
+Output:
+
+./a.out
+Press Ctrl+C to trigger SIGINT...
+^CCaught SIGINT (Interrupt signal)
+
+========================================================================================================
+*/
+
---
+++ HandsOn-2/10c.c
@@ -0,0 +1,38 @@
+/*
+========================================================================================================
+Name : 10a
+Author : Himanshu Rawat
+Description : Write a separate program using sigaction system call to catch the following signals.
+		a. SIGSEGV
+Date: 16 Sept, 2024.
+========================================================================================================
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void sigfpe_handler(int signo) {
+    printf("Caught SIGFPE (Floating-point exception)\n");
+    exit(1);
+}
+
+int main() {
+    struct sigaction sa;
+    sa.sa_handler = sigfpe_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    sigaction(SIGFPE, &sa, NULL);
+
+    int x = 1 / 0;
+}
+
+/*
+========================================================================================================
+Output:
+./a.out
+Caught SIGFPE (Floating-point exception)
+========================================================================================================
+*/
+
