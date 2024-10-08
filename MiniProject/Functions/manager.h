57b6c7687c6dd8f56b216412cd430ac7ad235cd9
himanshuraw <himanshurawat180@gmail.com>
2024-10-08 20:55:44 +0530

    Manager login

--- MiniProject/Functions/manager.h
+++ MiniProject/Functions/manager.h
@@ -1,4 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "../Structures/constants.h"
+
 #ifndef MANGER
 #define MANGER
+
+bool manager_handler(int client_socket);
+bool login_manager(int client_socket);
+
+bool manager_handler(int client_socket) {
+    if (login_manager(client_socket)) {
+        printf("Working with manager\n");
+    }
+}
+
+bool login_manager(int client_socket) {
+    char read_buffer[1000], write_buffer[1000], buffer[1000];
+    int read_bytes, write_bytes;
+
+    memset(read_buffer, 0, sizeof(read_buffer));
+    memset(write_buffer, 0, sizeof(write_buffer));
+
+    strcpy(write_buffer, MANAGER_LOGIN_PAGE);
+    strcat(write_buffer, USERNAME);
+
+    write_bytes = write(client_socket, write_buffer, strlen(write_buffer));
+    if (write_bytes == -1) {
+        perror("Write login page\n");
+        return false;
+    }
+
+    read_bytes = read(client_socket, read_buffer, sizeof(read_buffer));
+    if (read_bytes == -1) {
+        perror("Read Username\n");
+        return false;
+    }
+
+    if (read_bytes == 0) {
+        perror("No input\n");
+        return false;
+    }
+
+    if (strcmp(read_buffer, MANAGER_USERNAME) != 0) {
+        write_bytes =
+            write(client_socket, INVALID_USERNAME, strlen(INVALID_USERNAME));
+        return false;
+    }
+
+    write_bytes = write(client_socket, PASSWORD, strlen(PASSWORD));
+    if (write_bytes == -1) {
+        perror("Writing password in manager\n");
+        return false;
+    }
+
+    memset(read_buffer, 0, sizeof(read_buffer));
+    read_bytes = read(client_socket, read_buffer, sizeof(read_buffer));
+    if (read_bytes == -1) {
+        perror("Read password in manager\n");
+        return false;
+    }
+
+    char hashed_password[1000];
+    strcpy(hashed_password, crypt(read_buffer, SALT));
+
+    if (strcmp(hashed_password, MANAGER_PASSWORD) != 0) {
+        write_bytes =
+            write(client_socket, INVALID_PASSWORD, strlen(INVALID_PASSWORD));
+        return false;
+    }
+    return true;
+}
 
 #endif
