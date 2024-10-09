3af8c5e59d8d692d7200e0e666507c36497cf1bb
himanshuraw <himanshurawat180@gmail.com>
2024-10-09 16:52:35 +0530

    seperate for transfer in and transfer out

--- MiniProject/Structures/transaction.h
+++ MiniProject/Structures/transaction.h
@@ -6,7 +6,7 @@
 struct Transaction {
     int transaction_id;
     int account_number;
-    int operation;  // 0: Deposit, 1: Withdraw, 2: Transfer
+    int operation;  // 0: Deposit, 1: Withdraw, 2: Transfer_in, 3: Transfer_out
     time_t transaction_time;
 };
 #endif
