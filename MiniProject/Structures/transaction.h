061f55ef117f960eff2bf9fcb0ac1171969d4a8f
himanshuraw <himanshurawat180@gmail.com>
2024-10-11 01:45:15 +0530

    modified transaction structure

--- MiniProject/Structures/transaction.h
+++ MiniProject/Structures/transaction.h
@@ -4,9 +4,10 @@
 #include <time.h>
 
 struct Transaction {
-    int transaction_id;
+    int id;
     int account_number;
+    float amount;
     int operation;  // 0: Deposit, 1: Withdraw, 2: Transfer_in, 3: Transfer_out
-    time_t transaction_time;
+    time_t time;
 };
 #endif
